fdae9f2a21fea74948c90b39db655fea25bab77e
2oh <shini12.new@gmail.com>
2026-07-20 22:02:44 +0900

    chore: release version 1.0.9

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from openpyxl import load_workbook
 CONFIG_FILE = "config.json"
 PREVIEW_CACHE_DIR = "preview_cache"
 APP_ICON_FILE = "assets/app_icon.ico"
-APP_VERSION = "v1.0.8"
+APP_VERSION = "v1.0.9"
 DEBUG = False
 
 PDF_FILE_NAME_COLOR = ft.Colors.BLUE_200
